aa8c7d3ccbc3bd91d8d7d1f0f31403321d8e71a0
Lyla <lyla@udacity.com>
2016-12-24 09:21:01 -0800

    Feat: Showing data, but with lots of bugs

--- app/src/main/java/android/example/com/squawker/SquawkAdapter.java
+++ app/src/main/java/android/example/com/squawker/SquawkAdapter.java
@@ -2,10 +2,13 @@ package android.example.com.squawker;
 
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -15,16 +18,32 @@ import android.widget.TextView;
 public class SquawkAdapter extends RecyclerView.Adapter<SquawkAdapter.SquawkViewHolder> {
 
     private Cursor mData;
+    private static SimpleDateFormat sDateFormat = new SimpleDateFormat("dd MMM");
 
     @Override
     public SquawkViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.squawk_list_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        SquawkViewHolder vh = new SquawkViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(SquawkViewHolder holder, int position) {
         mData.moveToPosition(position);
 
+        String message = mData.getString(MainActivity.COL_NUM_MESSAGE);
+        String author = mData.getString(MainActivity.COL_NUM_AUTHOR);
+
+        // Get the date for displaying
+        long dateMillis = mData.getLong(MainActivity.COL_NUM_DATE);
+        Date dateDate = new Date(dateMillis);
+        String date = sDateFormat.format(dateDate);
+
+        holder.messageTextView.setText(message);
+        holder.authorTextView.setText(author);
+        holder.dateTextView.setText(date);
     }
 
     @Override
